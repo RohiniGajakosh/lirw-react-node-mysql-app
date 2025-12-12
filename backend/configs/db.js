aea9760d60c0a8855c7353c094a653845ce628c3
RohiniGajakosh <139953977+RohiniGajakosh@users.noreply.github.com>
2025-12-12 15:05:35 +0530

    Remove commented section in db.js

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-<COMMENT
+
 const db = mysql.createConnection({
    host: 'database-1.ct60e6a2sl8x.ap-south-1.rds.amazonaws.com',
    port: '3306',
@@ -7,5 +7,5 @@ const db = mysql.createConnection({
    password: 'rohini123',
    database: 'myappdb'
 });
-COMMENT>>
+
 module.exports = db;
